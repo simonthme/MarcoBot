fa8e9ed15d2518eef32eb8ed5d41b7e8b64f17bc
Simon Thome <s.thome@dixeeapp.com>
2018-05-23 14:53:10 +0200

    handlers

--- handlers/restaurantHandler/restaurant.js
+++ handlers/restaurantHandler/restaurant.js
@@ -1,6 +1,7 @@
 const product_data = require("../../messenger/product_data");
 const apiMessenger = require("../../helpers/apiMessenger");
 const recommandationApi = require('../../helpers/recommendationApi');
+const restaurant = require('../../graphql/restaurant/query');
 const helper = require("../../helpers/helper");
 
 module.exports = (type, price, senderID) => {
@@ -10,17 +11,25 @@ module.exports = (type, price, senderID) => {
     },
     message: ''
   };
+  console.log('type : ' + typeof type);
+  console.log('price :' + price);
+  console.log('id : ' + senderID);
   messageData.message = product_data.fetchRestaurantsMessage;
   apiMessenger.sendToFacebook(messageData)
     .then(response => {
       delete messageData.message;
       messageData.sender_action = 'typing_on';
-      // if (response.status === 200)
-      //   return recommandationApi.sendQuery(q);
+      if (response.status === 200)
+        return apiMessenger.sendToFacebook(messageData);
+
     })
-    // .then(helper.delayPromise(1000))
+    .then(helper.delayPromise(1000))
     .then(response => {
-      console.log('DO REQUEST');
+      if (response.status === 200)
+        return recommandationApi.sendQuery(restaurant.queryRestaurantsByPriceAndType(senderID, type, price, 0));
+    })
+    .then(res => {
+      console.log(res);
     })
     .catch(err => {
       console.log(err);
