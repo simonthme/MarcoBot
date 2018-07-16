97cab608dae77e74f07ef7782730adf2274fdd08
Simon Thome <s.thome@dixeeapp.com>
2018-07-16 15:23:23 +0200

    price

--- handlers/restaurantHandler/priceIndex.js
+++ handlers/restaurantHandler/priceIndex.js
@@ -5,12 +5,12 @@ module.exports = (payload, type, senderID) => {
     case 'ONE':
       restaurantHandler(type, 1, senderID);
       break;
-    case 'TWO':
+    case 'TWO-THREE':
       restaurantHandler(type, 2, senderID);
       break;
-    case 'THREE':
-      restaurantHandler(type, 3, senderID);
-      break;
+    // case 'THREE':
+    //   restaurantHandler(type, 3, senderID);
+    //   break;
     case 'FOUR':
       restaurantHandler(type, 4, senderID);
       break;
