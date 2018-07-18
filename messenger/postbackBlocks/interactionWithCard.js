196a5173a93a605af3c11ac2547e03a75cc1f2af
corentin-glt <gillot.corentin@gmail.com>
2018-07-18 13:41:18 +0200

    change message after stop chat

--- messenger/postbackBlocks/interactionWithCard.js
+++ messenger/postbackBlocks/interactionWithCard.js
@@ -110,7 +110,6 @@ const _createGoing = (senderID, userID, eventID, eventName, resultat) => {
               })
               .then(helper.delayPromise(2000))
               .then(response => {
-              
                 if (response.status === 200 && resultat.tips !== null
                   && typeof resultat.tips !== 'undefined' && resultat.tips.length > 0){
                   return sendMessage(senderID, {text: resultat.tips}, "RESPONSE")
