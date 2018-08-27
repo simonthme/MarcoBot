0cb12591710acb7ee1a771b834f85b75a99b5c34
Simon Thome <s.thome@dixeeapp.com>
2018-08-27 20:12:02 +0200

    referral event

--- controllers/messageWebhook.js
+++ controllers/messageWebhook.js
@@ -5,6 +5,7 @@ const processMessage = require('../helpers/processMessage');
 const receivedPostback = require('../helpers/receivedPostback');
 const receivedQuickReply = require('../helpers/receivedQuickReply');
 const receiveLocation = require('../helpers/receiveLocation');
+const axios = require('axios');
 
 
 module.exports = (req, res) =>  {
@@ -17,6 +18,28 @@ module.exports = (req, res) =>  {
           } else {
             processMessage(event);
           }
+        } else if (event.referral) {
+          console.log(event.referral);
+          axios.post('https://graph.facebook.com/224098718181615/activities', {
+            event: 'CUSTOM_APP_EVENTS',
+            custom_events: JSON.stringify([
+              {
+                _eventName: event.referral.ref,
+              }
+            ]),
+            advertiser_tracking_enabled: 1,
+            application_tracking_enabled: 1,
+            extinfo: JSON.stringify(['mb1']),
+            page_id: event.recipient.id,
+            page_scoped_user_id: event.sender.id
+          })
+            .then(response => {
+              console.log(response.data);
+            })
+            .catch(err => {
+              console.log(err);
+              console.log(err.response.data.error);
+            })
         } else {
           if (event.postback) {
             receivedPostback(event);
